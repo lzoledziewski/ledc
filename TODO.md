f72eb651f12bb17576a748bb06187545d7210f5b
lzoledziewski <l.zoledziewski@gmail.com>
2021-04-17 11:14:29 +0200

    sync

--- TODO.md
+++ TODO.md
@@ -3,3 +3,5 @@ Check DALI + FASTLed combination:
 - FASTLED RMT BUILTIN Driver + RMT RX for DALI
 - FASTLED RMT Driver + GPIO for DALI
 - FASTLED RMT Driver + Custom RMT Rx for DALI
+
+Figure out how to handle reception of backward frame's
--- src/dali_structures.h
+++ src/dali_structures.h
@@ -5,6 +5,33 @@
 #define DALI_CMD_SPECIAL_OFFSET 256u
 #define DALI_CMD_APP_EXT_OFFSET 224u
 
+
+typedef struct 
+{
+    /* data */
+}DT0_RAM_Vars;
+
+typedef struct 
+{
+    /* data */
+}DT6_RAM_Vars;
+
+typedef struct 
+{
+    /* data */
+}DT8_RAM_Vars;
+typedef union
+{
+    DT6_RAM_Vars dt6;
+    DT8_RAM_Vars dt8;
+}RAM_App_ext;
+typedef struct 
+{
+    DT0_RAM_Vars dt0;
+    RAM_App_ext appext;
+}RAM_Vars;
+
+
 typedef enum
 {
     e_SHORT_ADDR,
@@ -14,6 +41,19 @@ typedef enum
     e_SPECIAL,
     e_RESERVED
 } eAddressMode;
+
+
+
+typedef struct 
+{
+    void *controller; /*something to manage lights*/
+    void *dali_data; /*pointer to DALI variables, might be index or sth*/
+    void *callbacks;
+}dali_device_t;
+typedef struct
+{
+    dali_device_t devices[64];
+}dali_ctx_t;
 
 
 #define AUTO_ENUM
